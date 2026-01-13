6cbce8778ec6cc8a63a8ac13f428a2bd91521ade
Vinod Kumar <128352464+vinodkumarjaipal@users.noreply.github.com>
2026-01-13 13:58:41 +0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (65.32%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue1.offer(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        // Move all elements except last to queue2
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        int top = queue1.poll();
+        // Swap queues
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        return top;
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+        int top = queue1.poll();
+        queue2.offer(top); // keep it in queue
+        // Swap queues
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+        return top;
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
